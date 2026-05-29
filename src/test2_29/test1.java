3f2d1ed841ce26b9f487b3a591122518fd46a3d0
y-plum-yellow-rain <2558105869@qq.com>
2026-05-29 14:20:02 +0800

    测试提交

---
+++ src/test2_29/test1.java
@@ -0,0 +1,5 @@
+package test2_29;
+
+public class test1 {
+
+}
--- src/test5_24/test1.java
+++
@@ -1,7 +0,0 @@
-package test5_24;
-
-public class test1 {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
-}
