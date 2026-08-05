16c6cc1e663ebb5164623d4b96270d63a1bcdea0
y-plum-yellow-rain <2558105869@qq.com>
2026-08-05 20:05:52 +0800

    测试提交

---
+++ src/test8/Demo2.java
@@ -0,0 +1,12 @@
+package test8;
+
+public class Demo2 {
+    public static void main(String[] args) {
+
+            // 创建线程对象
+            Mythread2 t = new Mythread2();
+            // 开启新线程运行
+            t.start();
+        }
+    }
+
