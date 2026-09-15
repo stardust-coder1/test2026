8fcd16d265c7403c7254a6121e23a1d6e3a13ccf
y-plum-yellow-rain <2558105869@qq.com>
2026-09-15 15:14:37 +0800

    测试提交

---
+++ src/test_9/test9_15.java
@@ -0,0 +1,15 @@
+package test_9;
+
+public class test9_15 {
+    public static void eat(){
+        System.out.println("eat...");
+    }
+    public static void study(){
+        System.out.println("study...");
+    }
+
+    public static void main(String[] args) {
+        test9_15.eat();
+        test9_15.study();
+    }
+}
