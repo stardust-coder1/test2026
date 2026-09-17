919f2787dd0672fe120d4ef673a0715a8e50f494
y-plum-yellow-rain <2558105869@qq.com>
2026-09-17 16:18:20 +0800

    测试提交

---
+++ src/test_9/ExpectionDemo.java
@@ -0,0 +1,17 @@
+package test_9;
+
+public class ExpectionDemo {
+    public static void main(String[] args) {
+        try {
+            int []arr = {1,2,3};
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+            }
+        }catch (Exception e){
+            System.out.println("捕获到的异常并处理。。。。。。。");
+        }finally{
+            System.out.println("释放资源");
+        }
+        System.out.println("11111111111111111");
+    }
+}
